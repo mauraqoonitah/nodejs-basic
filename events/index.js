c2e5cc3ce24749ee6f387e8b61621f339baf8a81
mauraqp <mauraqoonitah@gmail.com>
2021-05-01 02:40:26 +0700

    innitial commit

---
+++ events/index.js
@@ -0,0 +1,17 @@
+// TODO 1 : Buat atau impor variabel EventEmitter dari core module events.
+const { EventEmitter } = require('events');
+
+// TODO 2 :  Buat variabel myEmitter yang merupakan instance dari EventEmitter.
+const myEventEmitter = new EventEmitter();
+
+// TODO 3: Tentukan birthdayEventListener sebagai aksi ketika event ‘birthday’ dibangkitkan pada myEmitter.
+
+const birthdayEventListener = (name) => {
+    console.log(`Happy birthday ${name}!`);
+}
+
+myEventEmitter.on('birthday', birthdayEventListener);
+
+
+// TODO 4 : Bangkitkanlah event ‘birthday’ pada myEmitter dengan method emit() dan beri nilai argumen listener dengan nama Anda.
+myEventEmitter.emit('birthday', 'mora');
